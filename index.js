9910b68b676a8c402f7b27f28a885df1f3b1ae42
chayatep <chayatep@chayateps-MacBook-Pro.local>
2017-10-25 01:23:37 +0700

    test 01

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+var bot = new builder.UniversalBot(connector);
+
+server.post('api/messages' , connector.listen());
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
 });
